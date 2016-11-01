0cd8b8932456426796754fd51dc591d6eb687506
radiolok <radiolok@yandex.ru>
2016-11-01 22:48:06 +0300

    ADD: vim and tmux installation checking

--- install_rc.sh
+++ install_rc.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+#lets check does vim exist, if no - install it:
+
+if ! which vim; then
+    sudo apt -y install vim
+fi
+
+if ! which tmux; then
+    sudo apt -y install tmux 
+fi
+
+
 #this script install all config files into user
-cp -v ./vimrc ~/.vimrc
+cp -v vimrc ~/.vimrc
 
 #copy tmux.conf config file
 
-cp -v ./tmux.conf ~/.tmux.conf
+cp -v tmux.conf ~/.tmux.conf
 
 
 tmux source ~/.tmux.conf
